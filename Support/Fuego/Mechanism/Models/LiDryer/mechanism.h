f218c85bc48a3e323c72973ff25e2f9d8ffee465
Marc Day <MSDay@lbl.gov>
2018-12-05 20:16:36 -0800

    cleanup nspec handling a bit

--- Support/Fuego/Mechanism/Models/LiDryer/mechanism.h
+++ Support/Fuego/Mechanism/Models/LiDryer/mechanism.h
@@ -1,25 +1,24 @@
 #ifndef MECHANISM_h
 #define MECHANISM_h
 #if 0
-//last_spec 8
-/* Species Indexes
-0  H2
-1  O2
-2  H2O
-3  H
-4  O
-5  OH
-6  HO2
-7  H2O2
-8  N2
+/* Elements 
+0 H
+1 O
+2 N
+*/
+/* Species 
+0 H2
+1 O2
+2 H2O
+3 H
+4 O
+5 OH
+6 HO2
+7 H2O2
+8 N2
 */
 #endif
-
-#define NSP 9
-#define NN 10
-#define FWD_RATES 21
-#define REV_RATES 21
-#define PRES_MOD_RATES 6
-
+#define NUM_ELEMENTS 3
+#define NUM_SPECIES 9
+#define NUM_REACTIONS 21
 #endif
-
