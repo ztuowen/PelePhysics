1814068982b4c9ec437aeaf1ed2dc84af327164e
Jon Rood <jon.rood@nrel.gov>
2018-12-31 15:29:05 -0700

    Adding drm19 F90 file generated by Fortran Fuego.

---
+++ Support/Fuego/Mechanism/Models/drm19/make-drm19F.sh
@@ -0,0 +1,23 @@
+
+CHEMINP=drm19.dat
+THERMINP=thermo12.dat
+FINALFILE=drm19.F90
+
+FMC=${PELE_PHYSICS_HOME}/Support/Fuego/Pythia/products/bin/fmc.py
+HEADERDIR=${PELE_PHYSICS_HOME}/Support/Fuego/Mechanism/Models/header
+
+CHEMLK=chem.asc
+LOG=chem.log
+CHEMC=chem.F90
+
+${FUEGO_PYTHON} ${FMC} -mechanism=${CHEMINP} -thermo=${THERMINP}  -name=${CHEMC}
+echo Compiling ${FINALFILE}...
+#cat ${CHEMC} \
+#          ${HEADERDIR}/header.start\
+#          ${HEADERDIR}/header.mec   ${CHEMINP}\
+#          ${HEADERDIR}/header.therm ${THERMINP}\
+#          ${HEADERDIR}/header.end > ${FINALFILE}
+cat ${CHEMC} > ${FINALFILE}
+rm -f ${CHEMC} ${CHEMLK} ${LOG} 
+sed -i.bak '/\/\*/d' ${FINALFILE}
+rm ${FINALFILE}.bak
