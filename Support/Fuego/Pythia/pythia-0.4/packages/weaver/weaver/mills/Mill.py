89f0466ef77ecd577b09d2c9b41ad44170e55455
Anne Felden <Enna@lbl.gov>
2019-06-24 23:14:15 -0700

    [GPU] Modify CPickler to generate mechanism.h content and Mill and FMC to print it in mechanism.h. Also clean CPickler from gpu "__device__" stuff. Update LiDryer

--- Support/Fuego/Pythia/pythia-0.4/packages/weaver/weaver/mills/Mill.py
+++ Support/Fuego/Pythia/pythia-0.4/packages/weaver/weaver/mills/Mill.py
@@ -28,6 +28,7 @@ class Mill(Stationery, Indenter):
             #self._renderDocumentGPU(document)
             self._renderHeader_CHOP(document)
             self._renderDocument_CHOP(document)
+            self._renderMechHeader_CHOP(document)
         self._end()
 
         return self._rep
