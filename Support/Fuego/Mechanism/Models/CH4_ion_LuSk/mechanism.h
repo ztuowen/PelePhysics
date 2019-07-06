d13e2d304fb0d5e1a73ccc8f5663487c5026670b
Lucas Esclapez <lesclapez@lbl.gov>
2019-07-05 23:10:39 -0700

    [feature_efield] Update the CH4ion mech with latest update from AF.

---
+++ Support/Fuego/Mechanism/Models/CH4_ion_LuSk/mechanism.h
@@ -0,0 +1,61 @@
+#ifndef MECHANISM_h
+#define MECHANISM_h
+#if 0
+/* Elements
+0  O
+1  H
+2  C
+3  N
+4  E
+*/
+/* Species
+0  H2
+1  H
+2  O
+3  O2
+4  OH
+5  H2O
+6  HO2
+7  H2O2
+8  C
+9  CH
+10  CH2
+11  CH2(S)
+12  CH3
+13  CH4
+14  CO
+15  CO2
+16  HCO
+17  CH2O
+18  CH2OH
+19  CH3O
+20  CH3OH
+21  C2H2
+22  C2H3
+23  C2H4
+24  C2H5
+25  C2H6
+26  HCCO
+27  CH2CO
+28  CH2CHO
+29  N2
+30  H3Op
+31  HCOp
+32  C2H3Op
+33  CH5Op
+34  OHn
+35  On
+36  O2n
+37  CO3n
+38  HCO3n
+39  E
+*/
+#endif
+#define NUM_ELEMENTS 5
+#define NUM_SPECIES 40
+#define NUM_REACTIONS 246
+
+#define NUM_FIT 4
+#endif
+
+/* End of file  */
