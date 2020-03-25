d69ad321df96f2b60ebc49e91d2a4697867b856c
Bruce Perry <baperry@princeton.edu>
2020-03-25 14:11:34 -0600

    Skeletal jp8 mechanism in Fuego format

---
+++ Support/Fuego/Mechanism/Models/jp8-lu-skl/mechanism.h
@@ -0,0 +1,61 @@
+#ifndef MECHANISM_h
+#define MECHANISM_h
+
+#if 0
+/* Elements
+0  O
+1  H
+2  C
+3  N
+4  AR
+*/
+#endif
+
+/* Species */
+#define POSF10325_ID 0
+#define C2H4_ID 1
+#define CH4_ID 2
+#define H2_ID 3
+#define C3H6_ID 4
+#define C4H81_ID 5
+#define iC4H8_ID 6
+#define C6H6_ID 7
+#define C6H5CH3_ID 8
+#define H_ID 9
+#define O_ID 10
+#define OH_ID 11
+#define HO2_ID 12
+#define H2O_ID 13
+#define H2O2_ID 14
+#define O2_ID 15
+#define CH2_ID 16
+#define CH2D_ID 17
+#define CH3_ID 18
+#define HCO_ID 19
+#define CH2O_ID 20
+#define CH3O_ID 21
+#define CO_ID 22
+#define CO2_ID 23
+#define C2H2_ID 24
+#define C2H3_ID 25
+#define C2H5_ID 26
+#define C2H6_ID 27
+#define HCCO_ID 28
+#define CH2CO_ID 29
+#define CH2CHO_ID 30
+#define aC3H5_ID 31
+#define C6H5_ID 32
+#define C6H5CH2_ID 33
+#define C6H4O2_ID 34
+#define C6H5CO_ID 35
+#define C6H5CHO_ID 36
+#define N2_ID 37
+
+#define NUM_ELEMENTS 5
+#define NUM_SPECIES 38
+#define NUM_REACTIONS 185
+
+#define NUM_FIT 4
+#endif
+
+/* End of file  */
